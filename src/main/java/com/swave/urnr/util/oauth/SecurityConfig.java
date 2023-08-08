4aba822f57ae4c3be10b2314ed7103a439766d87
root <tmdtjq0116@naver.com>
2023-08-08 15:58:20 +0900

    commit: loginStatus websocket
    type: feat
    body: loginStatus websocket으로 변경 추가

--- src/main/java/com/swave/urnr/util/oauth/SecurityConfig.java
+++ src/main/java/com/swave/urnr/util/oauth/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -24,7 +25,7 @@ public class SecurityConfig{
 
 
 
-    public static final String FRONT_URL = "http://localhost:5173";
+    public static final String FRONT_URL = "http://localhost:3000";
 
     private final CorsFilter corsFilter;
 
