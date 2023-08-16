cfd1f10cbca7df5c008cf9d59b23ff0372d9aef5
cmilk18 <43445697+cmilk18@users.noreply.github.com>
2023-08-17 04:54:48 +0900

    Update UserServiceImpl.java

--- src/main/java/com/swave/urnr/user/service/UserServiceImpl.java
+++ src/main/java/com/swave/urnr/user/service/UserServiceImpl.java
@@ -237,21 +237,36 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public ResponseEntity<SseEmitter> getTokenByOauth(String code, String provider) {
         log.info("hi1 : "+new Date());
+        System.out.println("SWAVE1 :"+new Date());
         OauthToken oauthToken = oAuthService.getOauthAccessToken(code, provider);
         log.info("hi2 : "+new Date());
+
+        System.out.println("SWAVE2 :"+new Date());
         String jwtToken = oAuthService.getTokenByOauth(oauthToken.getAccess_token(), provider);
         log.info("hi3 : "+new Date());
+
+        System.out.println("SWAVE3 :"+new Date());
         HttpHeaders headers = new HttpHeaders();
         log.info("hi4 : "+new Date());
+
+        System.out.println("SWAVE4 :"+new Date());
         headers.add(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
         log.info("hi5 : "+new Date());
 
+        System.out.println("SWAVE5 :"+new Date());
+
         Long id = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(jwtToken).getClaim("id").asLong();
         log.info("hi6 : "+new Date());
+
+        System.out.println("SWAVE6 :"+new Date());
         SseEmitter sseEmitter = sseEmitterService.subscribeEmitter(String.valueOf(id), jwtToken);
         log.info("hi7 : "+new Date());
+
+        System.out.println("SWAVE7 :"+new Date());
         kafkaService.createTopic(id.toString());
         log.info("hi8 : "+new Date());
+
+        System.out.println("SWAVE8 :"+new Date());
 
         return  ResponseEntity.ok().body(sseEmitter);
     }
