c919cce1729ead42a6e9011ecea5d31d952f1b82
cmilk18 <43445697+cmilk18@users.noreply.github.com>
2023-08-17 00:27:37 +0900

    chatgpt

--- src/main/java/com/swave/urnr/chatgpt/requestdto/ChatGPTQuestionRequestDTO.java
+++ src/main/java/com/swave/urnr/chatgpt/requestdto/ChatGPTQuestionRequestDTO.java
@@ -2,12 +2,14 @@ package com.swave.urnr.chatgpt.requestdto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.io.Serializable;
 @Getter
 @ApiIgnore
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChatGPTQuestionRequestDTO implements Serializable {
     //    private String question;
     private String question;
