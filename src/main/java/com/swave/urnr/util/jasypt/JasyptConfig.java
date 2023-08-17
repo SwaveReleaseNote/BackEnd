dd66d2cf3063f68001a25155c5093bf3299eb272
cmilk18 <43445697+cmilk18@users.noreply.github.com>
2023-08-17 19:52:26 +0900

    Update JasyptConfig.java

--- src/main/java/com/swave/urnr/util/jasypt/JasyptConfig.java
+++ src/main/java/com/swave/urnr/util/jasypt/JasyptConfig.java
@@ -2,9 +2,9 @@ package com.swave.urnr.util.jasypt;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.junit.Test;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
